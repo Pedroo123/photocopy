017c078889b79e749c5a9effcd1acd6831ddabfc
Andre G <andre.gros@ws-verita.sede>
2018-09-04 13:31:30 -0300

    Criado requisição post e put para a api de professor.

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ var mongoose = require('mongoose');
 var Teacher = require('./app/models/teacher');
 
 //mongoDB local
-mongoose.conect('mongodb://localhost/photocopy');
+mongoose.conect('mongodb://localhost/photocopy', {
+    useMongoClient:true
+});
 
 //Configuração da variavel app para usar o body-parse()
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,13 +26,38 @@ app.use(bodyParser.json());
 //Definindo a porta onde sera executada a api
 var port = process.env.port || 8000;
 
-//Criando uma instancia das Rotas via Express
+//Criando uma instancia das Rotas via Express=======
+
 var router = express.Router();
+
+router.use(function(req, res){
+    console.log('Algo esta acontecendo aqui...');
+    next();
+});
 
 //Rota exemplo
 router.get('/', function(req, res){
     res.json({ message: 'teste get' })
 });
+
+//API's:=============================================
+
+router.route('/teacher')
+
+    .post(function(req, res){
+        let teacher = new Teacher();
+
+        teacher.matriculation = req.body.matriculation;
+        teacher.name = req.body.name;
+
+        teacher.save(function(error){
+            if (error) {
+                res.send('Erro ao salvar o professor' + error);
+            } else {
+                res.json({ message: 'Professor cadastrado com sucesso!' });
+            }
+        });
+    });
 
 //Definindo um padrão das rotas prefixadas: /api
 app.use('/api', router);
